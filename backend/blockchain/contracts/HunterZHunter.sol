6a1b6908f6de9bddb1ac12f3705c8da980a97f41
Rich Warner <rich@RW-SurfacePro7>
2023-02-26 11:32:33 -0700

    contract events

---
+++ backend/blockchain/contracts/HunterZHunter.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract HunterZHunter {
+    struct Hunt {
+        string huntId;
+        string name;
+        uint prize;
+        uint endTime;
+        bytes32 target;
+    }
+
+    mapping (string => Hunt) hunts;
+
+    event HuntAdded(string huntId, string name, uint prize, uint endTime, bytes32 target);
+    event PrizeWon(string huntId, address winner, uint prize);
+
+    function addHunt(string memory huntId, string memory name, uint prize, uint endTime, bytes32 target) public {
+        Hunt storage newHunt = hunts[huntId];
+        newHunt.huntId = huntId;
+        newHunt.name = name;
+        newHunt.prize = prize;
+        newHunt.endTime = endTime;
+        newHunt.target = target;
+        emit HuntAdded(huntId, name, prize, endTime, target);
+    }
+
+    function verifyAndAwardPrize(address winner, bytes memory proof) public {
+        // call another contract to do the verification
+        bool verified = verifyProof(winner, proof);
+        require(verified, "Proof not verified");
+
+        // transfer prize ETH to the winner
+        uint prize = hunts[huntId].prize;
+        hunts[huntId].prize = 0;
+        (bool success, ) = winner.call{value: prize}("");
+        require(success, "Transfer failed");
+
+        emit PrizeWon(huntId, winner, prize);
+    }
+
+    function verifyProof(address winner, bytes memory proof) private returns (bool) {
+        // call another contract to do the verification
+        // implementation omitted
+    }
+}
