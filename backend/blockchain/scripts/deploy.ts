e69693a1d97f5e87f0e91dd49fd2c9a547142298
chmilevfa <17650184+ChmilevFA@users.noreply.github.com>
2023-02-28 21:14:49 +0100

    improved validations for HunterZHunter + fix deployment script

--- backend/blockchain/scripts/deploy.ts
+++ backend/blockchain/scripts/deploy.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 
 async function main() {
   const HzhContract = await ethers.getContractFactory("HunterZHunter");
-  const contract = await HzhContract.deploy();
+  const contract = await HzhContract.deploy("0x0000000000000000000000000000000000000000");
 
   await contract.deployed();
 
