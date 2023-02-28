d66e55b79bf990dad5f451dc376782981f1efa9e
lancenonce <lancendavis47@gmail.com>
2023-02-27 22:47:27 -0700

    Deleted foundry project and completed verifyProof function

--- backend/blockchain/contracts/HunterZHunter.sol
+++ backend/blockchain/contracts/HunterZHunter.sol
@@ -42,6 +42,9 @@ contract HunterZHunter {
 
     function verifyProof(address winner, bytes memory proof) private returns (bool) {
         // call another contract to do the verification
+        Verifier verifier = new Verifier(0x000000); // insert address of Verifier contract
+        verifier.verify(winner, proof);
         // implementation omitted
+        return verifier ? true : false;
     }
 }
