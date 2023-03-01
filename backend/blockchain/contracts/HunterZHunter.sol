c4179c419ead8ff23c266f65a28de3a018cca008
chmilevfa <17650184+ChmilevFA@users.noreply.github.com>
2023-03-01 06:57:01 +0100

    add image reference to the smart contract

--- backend/blockchain/contracts/HunterZHunter.sol
+++ backend/blockchain/contracts/HunterZHunter.sol
@@ -7,19 +7,19 @@ contract HunterZHunter {
     struct Hunt {
         string huntId;
         string name;
+        string description;
         uint prize;
         uint endTime;
+        string imageReference;
         string target;
-        string description;
     }
 
     address payable public owner;
     address public verifier;
     mapping (string => Hunt) hunts;
     mapping (string => bool) huntsSaved;
-    string IPFS_HASH = ;
 
-    event HuntAdded(string huntId, string name, uint prize, uint endTime, string target, string description);
+    event HuntAdded(string huntId, string name, string description, uint prize, uint endTime, string imageReference, string target);
     event PrizeWon(string huntId, address winner, uint prize);
 
     constructor(address _verifier) {
@@ -27,20 +27,21 @@ contract HunterZHunter {
         verifier = _verifier;
     }
 
-    function addHunt(string memory huntId, string memory name, uint endTime, string memory target, string memory description) public payable {
+    function addHunt(string memory huntId, string memory name, string memory description, uint endTime, string memory imageReference, string memory target) public payable {
         require(!huntsSaved[huntId], "hunt with provided id already exists");
         require(msg.value > 0, "prize cannot be zero");
 
         Hunt storage newHunt = hunts[huntId];
         newHunt.huntId = huntId;
         newHunt.name = name;
+        newHunt.description = description;
         newHunt.prize = msg.value;
         newHunt.endTime = endTime;
+        newHunt.imageReference = imageReference;
         newHunt.target = target;
-        newHunt.description = description;
 
         huntsSaved[huntId] = true;
-        emit HuntAdded(huntId, name, msg.value, endTime, target, description);
+        emit HuntAdded(huntId, name, description, msg.value, endTime, imageReference, target);
     }
 
     function verifyAndAwardPrize(string memory huntId, address winner, bytes memory proof) public {
